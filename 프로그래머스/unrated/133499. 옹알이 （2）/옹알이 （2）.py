cfe9240211b9d15d27a1cef029da5e80f6b9e19f
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2023-08-23 00:12:31 +0900

    [unrated] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,13 +1,3 @@
 def solution(babbling):
-    pos_word = ["aya", "ye", "woo", "ma"]
-    cnt = 0
-    
-    for i in babbling:
-        for j in pos_word:
-            if not j * 2 in i:
-                i = i.replace(j, " ")
-        
-        if i.isspace():
-            cnt += 1
-            
-    return cnt
+    answer = 0
+    return answer
