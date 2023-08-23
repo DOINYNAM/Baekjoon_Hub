a90ce036a7ca83f6677d37266a034ddb653279b1
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2023-08-23 11:46:46 +0900

    Update 2016년.py

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,5 +1,5 @@
 def solution(a, b):
-    day_list = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
-    month_list = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day_list = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"] # 1/1 금요일 기준 요일별 인덱스 설정
+    month_list = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 월별 총 일 수
 
-    return day_list[(sum(month_list[:a-1]) + b) % 7]
+    return day_list[(sum(month_list[:a-1]) + b) % 7] # 일 수 계산 후 7로 나누어 day_list 인덱스에 맞추어 요일 출력
