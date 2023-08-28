1b1f599b07b68c2b4b3fe6f2ebd6abe383e4c18e
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2023-08-29 02:33:28 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,29 @@
+def solution(X, Y):
+    X = sorted(list(X))
+    Y = sorted(list(Y), reverse = True)
+    same_thing = []
+    
+    for i in range(len(X)):
+        while len(Y) > 0:
+            if X[i] == Y[-1]: # 반대 방향의 인덱스끼리 비교
+                same_thing.append(X[i])
+                Y.pop(-1)           
+                break
+            
+            else:
+                if X[i] < Y[-1]:
+                    break
+                
+                else:
+                    Y.pop(-1)
+                    
+    same_thing = "".join(sorted(same_thing, reverse = True))
+    
+    if len(same_thing) == 0:
+        return "-1"
+    
+    elif set(same_thing) == {"0"}:  # n개의 0으로만 구성된 경우, "0"으로 출력하기 위해 조회
+        return "0"
+        
+    else:
+        return same_thing
