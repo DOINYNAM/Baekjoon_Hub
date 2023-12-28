b1756587c4f6dc8dc1d1e4a27588c813ef5a5d74
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2023-12-28 20:14:52 +0900

    [Silver I] Title: 격자상의 경로, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -1,23 +1,36 @@
-# 목적 좌표에 도달하는 경우의 수 계산 함수
-def result(n, m): 
-  dp = [[0] * m for _ in range(n)]
-  for row in range(n):
-    for col in range(m):
-      if row == 0 or col == 0: # 첫 행, 열을 1로 채우기
-        dp[row][col] = 1
-      else:
-        dp[row][col] = dp[row - 1][col] + dp[row][col - 1] # 해당 칸을 왼쪽과 위쪽칸의 수를 더해 채우기
-  
-  return dp[n - 1][m - 1]
-
-# 실행 부분
 n, m, k = map(int, input().split())
-if k == 0: # 원이 없는 경우
-  print(result(n, m))
+dp = [[0] * m for _ in range(n)]
 
-else: # 원이 있는 경우
-  x, y = k // m + 1, k % m # 원이 있는 지점의 좌표 구하기
-  n, m = (n + 1) - x, (m + 1) - y # 원이 있는 지점 이후부터 목적 좌표까지의 좌표 크기 재설정
-  dp1, dp2 = result(x, y), result(n, m)
-  
-  print(dp1 * dp2) # 원이 있는 지점까지 도달하는 경우의 수  * 원이 있는 지점 제외 도달하는 경우의 수 = 원 좌표를 포함한 목적 좌표까지 도달하는 경우의 수
+if k == 0:
+    for i in range(n):
+        for j in range(m):
+            if i == 0 or j == 0:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    print(dp[n - 1][m - 1])
+
+else:
+    x = (k - 1) // m
+    y = (k - 1) % m
+
+    for i in range(x + 1):
+        for j in range(y + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    for i in range(x, n):
+        for j in range(y, m):
+            if i == x and j == y:
+                continue
+            if i == x:
+                dp[i][j] = 1
+            elif j == y:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    print(dp[x][y] * dp[n - 1][m - 1])
