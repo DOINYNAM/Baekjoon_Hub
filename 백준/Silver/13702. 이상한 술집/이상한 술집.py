96020d29406993b86575ade19027bc39d0700151
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2024-01-04 10:43:56 +0900

    [Silver III] Title: 이상한 술집, Time: 420 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.py
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.py
@@ -1,9 +1,3 @@
-# 백준 13702 이상한술집
-# 이분 탐색
-
-# start, end는 ml 지표
-# N 주전자 개수, K 친구들 수
-
 N, K = map(int, input().split())
 btl = [int(input()) for _ in range(N)]
 
@@ -12,16 +6,12 @@ end = max(btl)
 
 while start <= end:
   mid = (start + end) // 2 # 첫 기준 ml 설정
-  tmp = 0 # mid 용량으로 따른 잔의 개수 저장
-  
-  for i in btl:
-    tmp += i // mid # 각 주전자를 기준으로 mid 용량으로 따른 잔의 개수 저장
+  tmp = sum(i // mid for i in btl) # 각 주전자를 기준으로 mid 용량으로 따른 잔의 개수 저장
 
   if tmp >= K: # 잔의 개수가 많거나 충분할 때
     start = mid + 1
-    answer = mid
 
   else: # 잔의 개수가 모자를 때
     end = mid - 1
 
-print(answer)
+print(end)
