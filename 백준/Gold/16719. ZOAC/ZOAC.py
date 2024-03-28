23c1876de0285213196d8a0d93781a960a69e17a
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2024-03-28 20:08:49 +0900

    [Gold V] Title: ZOAC, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/16719. ZOAC/ZOAC.py
@@ -0,0 +1,24 @@
+# ex. showActivateStr("CAT", [True, True, False])는 True에 해당하는 문자인 "CA"를 반환함
+def showActivateStr(target, isActivate):
+    result = ""
+    for i in range(len(isActivate)):
+        if isActivate[i]:
+            result += target[i]
+    return result
+
+target = input()
+isActivate = [False] * len(target)
+
+for i in range(len(target)):
+    # isActivate값이 False인것 하나하나에 대해서 추가했을 때의 가능한 문자열들을 모아놓고 가장 앞서는 경우를 확인
+    tempResult = [] # (문자열, 그때 활성화 시킨 인덱스)
+    
+    for j in range(len(isActivate)):
+        if isActivate[j] == False:
+            isActivate[j] = True
+            tempResult.append((showActivateStr(target, isActivate), j))
+            isActivate[j] = False
+    
+    tempResult.sort() # tempResult의 가장 앞에는 (가장 앞서는 문자열, 그때 활성화 시킨 인덱스)가 될 것임
+    print(tempResult[0][0])
+    isActivate[tempResult[0][1]] = True
