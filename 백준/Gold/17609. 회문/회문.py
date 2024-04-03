1068c6bbd724321282ac843deb635cab5bc784b5
Nam Do In <87957221+DOINYNAM@users.noreply.github.com>
2024-04-03 14:49:28 +0900

    [Gold V] Title: 회문, Time: 156 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -3,17 +3,21 @@ def check_palindrome(word):
     back = len(word) - 1
     
     while front < back:
-        if word[front] != word[back]:
-            # 앞쪽 문자를 제거하는 경우
-            remove_front = word[front+1:back+1]
-            # 뒤쪽 문자를 제거하는 경우
-            remove_back = word[front:back]
-            if remove_front == remove_front[::-1] or remove_back == remove_back[::-1]:
-                return 1  # 한 문자를 제거하여 회문을 만들 수 있음
-            return 2  # 회문을 만들 수 없음
-        front += 1
-        back -= 1
+      if word[front] != word[back]:
+        remove_front = word[:front] + word[front + 1:]
+        if remove_front == remove_front[::-1]:
+          return 1
 
+        remove_back = word[:back] + word[back + 1:]
+        if remove_back == remove_back[::-1]:
+          return 1
+
+        else: # 회문 불가
+            return 2
+
+      front += 1
+      back -= 1
+  
     return 0  # 원래부터 회문임
 
 N = int(input())
